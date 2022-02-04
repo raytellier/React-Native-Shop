ef2a679e672741f9942a9324babf3b110c2a0a6f
raytellier <ray@frap.io>
2022-02-03 20:46:35 -0500

    Added redux and structed project

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { initFirebase } from './config/firebase';
+
+
+initFirebase();
+
 export default function App() {
   return (
     <View style={styles.container}>
--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,23 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import Constants from "expo-constants";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  databaseURL: Constants.manifest.extra.databaseURL,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId,
-  measurementId: Constants.manifest.extra.measurementId,
-};
+export const initFirebase = () => {
+  const firebaseConfig = {
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    databaseURL: Constants.manifest.extra.databaseURL,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId,
+    measurementId: Constants.manifest.extra.measurementId,
+  };
+  if(getApps.length === 0){
+    initializeApp(firebaseConfig);
+  }
+}
 
-initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const database = getFirestore();
