de75c3a647a847708c0713337e5ca7c7d0ac329e
raytellier <ray@frap.io>
2022-02-04 04:17:31 -0500

    Added bottom tab navigator

--- src/navigation/home/index.js
+++ src/navigation/home/index.js
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
+// Create bottom tab nav
+const Tab = createMaterialBottomTabNavigator();
+
+// Temp screen to populate navigation
+const EmptyScreen = () => {
+  return <View></View>;
+};
+
+// Bottom navigation
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Ayoooo Fam</Text>
-    </View>
+    <Tab.Navigator
+      barStyle={{ backgroundColor: "black" }}
+      initialRouteName="home"
+    >
+      <Tab.Screen
+        name="home"
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({ color }) => {
+            <Feather name="home" size={24} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="shop"
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({ color }) => {
+            <Feather name="search" size={24} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="profile"
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({ color }) => {
+            <Feather name="radio" size={24} color={color} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 }
