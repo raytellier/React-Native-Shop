beba4f3008c034b4b633915f86bcca48ef24b03d
raytellier <ray@frap.io>
2022-02-03 21:39:18 -0500

    init firebase properly

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,9 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import Constants from "expo-constants";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: Constants.manifest.extra.apiKey,
   authDomain: Constants.manifest.extra.authDomain,
   databaseURL: Constants.manifest.extra.databaseURL,
@@ -13,7 +13,9 @@ const firebaseConfig = {
   appId: Constants.manifest.extra.appId,
   measurementId: Constants.manifest.extra.measurementId,
 };
-initializeApp(firebaseConfig);
+
+//getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+//initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const database = getFirestore();
